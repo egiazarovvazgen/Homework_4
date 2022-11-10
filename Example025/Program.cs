2d224630aa1e32ec6b6de67fe137ae7975576d8a
Vazgen <egiazarov.vazgen@icloud.com>
2022-11-10 15:03:04 +0300

    Example025

---
+++ Example025/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+
+Console.WriteLine("Введите число А: ");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+int GetDegreeFromAToB(int a)
+{
+    int result = Convert.ToInt32(Math.Pow(numberA, numberB));
+    return result;
+}
+
+if (numberB > 0)
+{
+int result = GetDegreeFromAToB(numberA);
+Console.WriteLine("Результат: " + result);
+}
+else
+{
+    Console.WriteLine("Число B не является натуральным");
+}
