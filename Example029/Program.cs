0214b45706aa8438eafc5c05778eafac952dd4b9
Vazgen <egiazarov.vazgen@icloud.com>
2022-11-10 16:04:10 +0300

    Example029

---
+++ Example029/Program.cs
@@ -0,0 +1,25 @@
+﻿// Программа, которая задаёт массив из 8 целых чисел с клавиатуры и далее выводит массив на экран в одну строку
+
+int[] array = new int[8];
+
+void FillArray(int[] collection)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine("Введите число: ");
+        array[i] = int.Parse(Console.ReadLine()!);
+    }
+}
+
+void PrintArray(int[] col)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+    }
+}
+
+FillArray(array);
+Console.WriteLine("Массив: ");
+PrintArray(array);
+Console.WriteLine();
