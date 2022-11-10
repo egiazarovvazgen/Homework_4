3c290f73496c5d0b446055ac2a80dd837af1a37d
Vazgen <egiazarov.vazgen@icloud.com>
2022-11-10 15:33:54 +0300

    Example027

---
+++ Example027/Program.cs
@@ -0,0 +1,21 @@
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number < 0) number = Math.Abs(number);
+
+int GetSumFromDigits(int a)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        result = result + digit;
+        number = number / 10;
+    }
+    return result;
+}
+
+int result = GetSumFromDigits(number);
+Console.WriteLine("Результат: " + result);
